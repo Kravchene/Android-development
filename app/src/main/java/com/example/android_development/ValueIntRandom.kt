39b38cc767dee1f8483690fc4bb00f3531145045
Artur Lynko <alynko17@gmail.com>
2023-05-30 00:52:54 +0300

    review valueIntRandom

--- app/src/main/java/com/example/android_development/ValueIntRandom.kt
+++ app/src/main/java/com/example/android_development/ValueIntRandom.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ValueIntRandom: ViewModel() {
+    //Не проходили, но иметь публичную mutableLiveData не есть хорошая практика
     val  message:MutableLiveData<Int> by lazy {
         MutableLiveData<Int>()
     }
