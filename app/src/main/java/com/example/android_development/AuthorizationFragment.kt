5ed605b15a33f497c524ec46b6f79759e4565e98
Kravchene <kravchene@mail.ru>
2023-06-06 11:41:15 +0300

    fix

--- app/src/main/java/com/example/android_development/AuthorizationFragment.kt
+++ app/src/main/java/com/example/android_development/AuthorizationFragment.kt
@@ -5,10 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.NavController
-import androidx.navigation.fragment.findNavController
 import com.example.android_development.databinding.FragmentAuthorizationBinding
 
 class AuthorizationFragment : Fragment() {
@@ -17,20 +14,20 @@ class AuthorizationFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentAuthorizationBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        with (binding){
-            buttonLog.setOnClickListener{
-                if (emailOrUs.text.isEmpty()||password.text.isEmpty()){
-                }else{
-                    dataAccount.message.value =emailOrUs.text.toString()
+        with(binding) {
+            buttonLog.setOnClickListener {
+                if (emailOrUs.text!!.isEmpty() || password.text!!.isEmpty()) {
+                } else {
+                    dataAccount.message.value = emailOrUs.text.toString()
                     requireActivity().supportFragmentManager.beginTransaction()
-                        .replace(R.id.container,WebsiteFragment()).commit()
+                        .replace(R.id.container, WebsiteFragment()).commit()
                 }
             }
         }
--- app/src/main/java/com/example/android_development/MainActivity.kt
+++ app/src/main/java/com/example/android_development/MainActivity.kt
@@ -3,17 +3,16 @@ package com.example.android_development
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import androidx.activity.viewModels
-import androidx.lifecycle.LifecycleOwner
-import androidx.navigation.NavController
-import com.example.android_development.databinding.ActivityMainBinding
-class MainActivity : AppCompatActivity() {
 
+import androidx.activity.viewModels
+
+class MainActivity : AppCompatActivity() {
+    private val dataAccount:Account by viewModels()
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        dataAccount.message.observe(this,{})
 
         }
     }
